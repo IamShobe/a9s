d9ec34e7b9caf0645c2e2a2a79897a59b71ae522
Elran Shefer <elran777@gmail.com>
2021-06-25 21:43:58 +0300

    Seperated deploy jobs and fixed versionings

--- a9s/__init__.py
+++ a9s/__init__.py
@@ -3,6 +3,11 @@ from pkg_resources import get_distribution
 try:
     package = get_distribution(__package__)
     __version__ = package.version
+    if __version__ == '0':
+        __version__ = 'development'
+
+    else:
+        __version__ = 'v' + __version__
 
 except:
     __version__ = 'unknown'
