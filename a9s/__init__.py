7f60ac2d00b8d80a83d0854a5a3113cbf875494c
Elran Shefer <eshefer@grubhub.com>
2021-06-25 16:13:47 +0300

    Updated version grep

--- a9s/__init__.py
+++ a9s/__init__.py
@@ -1,7 +1,8 @@
-from importlib.metadata import version as get_version
+from pkg_resources import get_distribution
 
 try:
-    __version__ = get_version(__package__)
+    package = get_distribution(__package__)
+    __version__ = package.version
 
 except:
     __version__ = 'unknown'
