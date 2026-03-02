2e3b255a8192d3862cff3869d93e47ebff430a8f
Elran Shefer <31568840+IamShobe@users.noreply.github.com>
2026-03-02 12:16:38 +0200

    Update base_service.py (#31)
    Support aws endpoint

--- a9s/aws_resources/base_service.py
+++ a9s/aws_resources/base_service.py
@@ -8,6 +8,7 @@ from tepy.components.table import Table
 
 
 IS_LOCAL = os.environ.get('LOCAL', 'false').lower() == 'true'
+ENDPOINT_URL = os.environ.get('AWS_ENDPOINT', 'http://localhost:54321')
 
 
 class BaseService(Table, HUDComponent):
@@ -18,8 +19,8 @@ class BaseService(Table, HUDComponent):
 
     @cached_property
     def cloudwatch_client(self):
-        return boto3.client(service_name='cloudwatch', endpoint_url='http://localhost:54321' if IS_LOCAL else None)
+        return boto3.client(service_name='cloudwatch', endpoint_url=ENDPOINT_URL if IS_LOCAL else None)
 
     @cached_property
     def client(self):
-        return boto3.client(service_name=self.BOTO_SERVICE, endpoint_url='http://localhost:54321' if IS_LOCAL else None)
+        return boto3.client(service_name=self.BOTO_SERVICE, endpoint_url=ENDPOINT_URL if IS_LOCAL else None)
